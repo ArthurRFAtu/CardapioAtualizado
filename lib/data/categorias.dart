d6356b4dd33014d00f3b8177c5468bb60f45f785
Arthur Resende Ferreira <88728310+ArthurRFAtu@users.noreply.github.com>
2023-11-25 23:06:07 -0300

    Add files via upload

---
+++ lib/data/categorias.dart
@@ -0,0 +1,17 @@
+
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
+
+
+Future<List<dynamic>> dados_categoria() async{
+  final response = await http
+      .get(Uri.parse('http://localhost:3001/categoria'));
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    return jsonResponse;
+  } else {
+    throw Exception('Falha ao ler categorias!');
+  }
+}
+
---
+++ lib/utils/rotas.dart
@@ -0,0 +1,6 @@
+class Rotas {
+  static const HOME = '/';
+  static const PRODUTOS = "/tela_produtos";
+  static const CATEGORIA = "/categoria";
+  static const CATEGORIAPRODUTO = "/categoria/:id";
+}
